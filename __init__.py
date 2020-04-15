3e161ac602b7ea56249ef16bebeda758411db06d
ashlae blume <45116836+laelume@users.noreply.github.com>
2020-04-15 11:01:51 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+__all__ = ["boardstuff", "instances", "iteration", "motion", "gameplayer"]
+
+
+"""
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
+
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and
+# <--rejoin line--> not f.endswith('__init__.py')]
+
+"""
